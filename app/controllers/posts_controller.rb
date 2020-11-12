001b2719b263506cc0a7be4c1e52bdc3f45f30ed
GraceOyiza <graceoyiza13@gmail.com>
2020-11-12 16:37:40 +0100

    Add index methos and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!, only: %i[new create]
-
+    
+    def index
+        @post = Post.all
+    end
     
     def new
       @post = Post.new
