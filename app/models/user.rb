4d0f6ef348f0940c7c9c5801d512d836cfcd5578
Aye Daniel A <aadaniel108@gmail.com>
2020-11-12 16:57:01 +0100

    added validation to post and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  validates :username, presence:true,  uniqueness:true, length: {minimum:5, maximum:30}
+  validates :email, presence:true, :case_sensitive => false,  length:{minimum:5, maximum:30}
+  validates :password, presence:true,  length: {minimum:5, maximum:30}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
