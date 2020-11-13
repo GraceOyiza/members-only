bc0fc20e8f64d7687daf25f5c0098ad382c32362
GraceOyiza <graceoyiza13@gmail.com>
2020-11-13 17:46:37 +0100

    Fix rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
   def links_display
-    link_to('Logout', destroy_user_session_path, method: :delete) if user_signed_in?
+    links = ''
+    if user_signed_in?
+      links << link_to('Logout', destroy_user_session_path, method: :delete)
+      links << ' | '
+      links << link_to('Create Post', new_post_path)
+    else
+      links << link_to('Log In', new_user_session_path)
+      links << ' | '
+      links << link_to('Sign Up', new_user_registration_path)
+    end
+    links.html_safe
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,4 @@
 module PostsHelper
-  def params_helper
-    params.require(:post).permit(:title, :body)
-  end
-
   def post_author(post)
     post.user.username.to_s if user_signed_in?
   end
